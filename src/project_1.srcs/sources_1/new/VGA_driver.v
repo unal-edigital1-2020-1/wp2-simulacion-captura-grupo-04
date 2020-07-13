2aa4c5d234d3b2f0d0416b8c085a26b4595d6208
jcrojasda <55933743+jcrojasda@users.noreply.github.com>
2020-07-13 11:28:31 -0500

    Update VGA_driver.v

--- src/project_1.srcs/sources_1/new/VGA_driver.v
+++ src/project_1.srcs/sources_1/new/VGA_driver.v
@@ -1,3 +1,4 @@
+`timescale 10ns / 1ns		
 //////////////////////////////////////////////////////////////////////////////////
 // 
 // Create Date:    13:34:31 10/22/2019 
@@ -21,20 +22,20 @@ module VGA_Driver640x480 (
 	input  [11:0] pixelIn, 	// entrada del valor de color  pixel 
 	
 	output  [11:0] pixelOut, // salida del valor pixel a la VGA 
-	output  Hsync_n,		// señal de sincronización en horizontal negada
-	output  Vsync_n,		// señal de sincronización en vertical negada 
+	output  Hsync_n,		// se√±al de sincronizaci√≥n en horizontal negada
+	output  Vsync_n,		// se√±al de sincronizaci√≥n en vertical negada 
 	output  [9:0] posX, 	// posicion en horizontal del pixel siguiente
-	output  [8:0] posY 		// posicion en vertical  del pixel siguiente
+	output  [9:0] posY 		// posicion en vertical  del pixel siguiente
 );
 
-localparam SCREEN_X = 640; 	// tama�o de la pantalla visible en horizontal 
+localparam SCREEN_X = 640; 	// tamaÒo de la pantalla visible en horizontal 
 localparam FRONT_PORCH_X =16;  
 localparam SYNC_PULSE_X = 96;
 localparam BACK_PORCH_X = 48;
 localparam TOTAL_SCREEN_X = SCREEN_X+FRONT_PORCH_X+SYNC_PULSE_X+BACK_PORCH_X; 	// total pixel pantalla en horizontal 
 
 
-localparam SCREEN_Y = 480; 	// tama�o de la pantalla visible en Vertical 
+localparam SCREEN_Y = 480; 	// tamaÒo de la pantalla visible en Vertical 
 localparam FRONT_PORCH_Y =10;  
 localparam SYNC_PULSE_Y = 2;
 localparam BACK_PORCH_Y = 33;
@@ -42,7 +43,7 @@ localparam TOTAL_SCREEN_Y = SCREEN_Y+FRONT_PORCH_Y+SYNC_PULSE_Y+BACK_PORCH_Y; 	/
 
 
 reg  [9:0] countX;
-reg  [8:0] countY;
+reg  [9:0] countY;
 
 assign posX    = countX;
 assign posY    = countY;
@@ -76,3 +77,4 @@ always @(posedge clk) begin
 end
 
 endmodule
+
